74e0be8ef8db21c09036e84232a5935ba398b0a9
Sergio151410 <71415452+Sergio151410@users.noreply.github.com>
2020-10-20 19:40:54 -0500

    se acomodan pantallas

--- comercializadora/src/app/cliente/cliente.component.ts
+++ comercializadora/src/app/cliente/cliente.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.component.html',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
